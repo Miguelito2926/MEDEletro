8e98b937383dc5c7fb35318832cf29e3409a6b2f
Miguelito2926 <juniorprol@hotmail.com>
2023-12-10 20:30:21 -0300

    Criando Models e classe de Context responsavel pela conexão com o database

--- MEDEletro.ProductApi/Program.cs
+++ MEDEletro.ProductApi/Program.cs
@@ -1,11 +1,18 @@
+using MEDEletro.ProductApi.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var SqlServeConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(opt => 
+opt.UseSqlServer(SqlServeConnection));
+
 
 var app = builder.Build();
 
